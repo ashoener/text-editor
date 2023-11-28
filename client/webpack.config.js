30ed2869ecd59e621b88d378c8ffde8724dcd6d9
Andrew Shoener <a.b.shoener@gmail.com>
2023-11-28 15:36:04 -0600

    Add CSS loader to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -43,7 +43,12 @@ module.exports = () => {
     ],
 
     module: {
-      rules: [],
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
     },
   };
 };
